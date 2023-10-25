0bf07ced2a089fa1d0a5a2e926b7549f0996e370
Joshua Graham <joshuareynoldsgraham@gmail.com>
2023-10-25 18:19:54 -0500

    added the ability to look up specific creatures and items on the search route

--- src/server/routes/api/search.ts
+++ src/server/routes/api/search.ts
@@ -6,9 +6,10 @@ const router = express.Router();
 router.get("/creatures", (req, res) => {
     const pageSize = req.query.pageSize || 20;
     const page = req.query.page || 1;
+    const creature = req.query.creature ? `&name.en_US=${req.query.creature}` : "";
 
     fetch(
-        `https://us.api.blizzard.com/data/wow/search/creature?namespace=static-us&orderby=id&_page=${page}&_pageSize=${pageSize}&access_token=${req.user.access_token}`
+        `https://us.api.blizzard.com/data/wow/search/creature?namespace=static-us&orderby=id${creature}&_page=${page}&_pageSize=${pageSize}&access_token=${req.user.access_token}`
     )
         .then((res) => res.json())
         .then((data) => {
@@ -19,9 +20,10 @@ router.get("/creatures", (req, res) => {
 router.get("/items", (req, res) => {
     const pageSize = req.query.pageSize || 20;
     const page = req.query.page || 1;
+    const item = req.query.item ? `&name.en_US=${req.query.item}` : "";
 
     fetch(
-        `https://us.api.blizzard.com/data/wow/search/item?namespace=static-us&orderby=id&_page=${page}&_pageSize=${pageSize}&access_token=${req.user.access_token}`
+        `https://us.api.blizzard.com/data/wow/search/item?namespace=static-us&orderby=id${item}&_page=${page}&_pageSize=${pageSize}&access_token=${req.user.access_token}`
     )
         .then((res) => res.json())
         .then((data) => {
