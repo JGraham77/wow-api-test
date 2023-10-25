3f31a486646599d78411884338e96a0e1c6a3202
Joshua Graham <joshuareynoldsgraham@gmail.com>
2023-10-25 17:41:43 -0500

    FINALLY OFFICIALLY CONNECTED TO THE WOW API

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import searchRoute from "./search";
 
 const router = express.Router();
+
+router.use("/search", searchRoute);
 
 export default router;
--- src/server/routes/oauth/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import bnetRouter from "./bnet";
-
-const router = express.Router();
-
-router.use("/battlenet", bnetRouter);
-
-export default router;
