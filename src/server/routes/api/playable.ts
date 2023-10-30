d5bfb8e30c84154b78973e8d656bdcf0858c7166
Joshua Graham <joshuareynoldsgraham@gmail.com>
2023-10-30 11:51:45 -0500

    adding route for playable classes and a dynamic way to search each individual class by name instead of by their id number

--- src/server/routes/api/playable.ts
+++ src/server/routes/api/playable.ts
@@ -37,4 +37,39 @@ router.get("/races/:term", (req, res) => {
         });
 });
 
+router.get("/classes", (req, res) => {
+    fetch(
+        `https://us.api.blizzard.com/data/wow/playable-class/index?namespace=static-classic-us&locale=en_US&access_token=${req.user.access_token}`
+    )
+        .then((res) => res.json())
+        .then((data) => {
+            res.json({ happyGreeting: "fook u", data });
+        });
+});
+
+router.get("/classes/:term", (req, res) => {
+    fetch(
+        `https://us.api.blizzard.com/data/wow/playable-class/index?namespace=static-classic-us&locale=en_US&access_token=${req.user.access_token}`
+    )
+        .then((res) => res.json())
+        .then((data) => {
+            const pClass = data.classes.find(
+                // @ts-ignore
+                (c) => c.name.toLowerCase().replace(" ", "") === req.params.term.toLowerCase().replace(" ", "")
+            );
+            //@ts-ignore
+            const pClasses = data.classes.map((c) => c.name);
+            if (!pClass || !pClass.name || !pClass.id) {
+                return res.status(404).json({ message: "Not a valid race, fook u get it right", pClasses });
+            }
+            fetch(
+                `https://us.api.blizzard.com/data/wow/playable-class/${pClass.id}?namespace=static-classic-us&locale=en_US&access_token=${req.user.access_token}`
+            )
+                .then((res) => res.json())
+                .then((data) => {
+                    res.json({ happyGreeting: "fook u", data });
+                });
+        });
+});
+
 export default router;
